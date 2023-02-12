b01019c69416e38eca73cea8d91c1e1eaf70d24b
AlexKMarshall <alexmarshall@qogita.com>
2023-02-12 13:40:54 +0000

    chore: switch storybook from webpack to vite

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,45 @@
-import type { StorybookConfig } from '@storybook/react-webpack5';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
+    "../app/**/*.stories.@(js|jsx|ts|tsx)",
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "docs": {
-    "autodocs": "tag"
-  }
+  docs: {
+    autodocs: "tag",
+  },
 };
+
 export default config;
+
+// import type { StorybookConfig } from '@storybook/react-vite';
+
+// const config: StorybookConfig = {
+//   "stories": [
+//     "../stories/**/*.mdx",
+//     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+//   ],
+//   "addons": [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions"
+//   ],
+//   "framework": {
+//     "name": "@storybook/react-vite",
+//     "options": {}
+//   },
+//   "docs": {
+//     "autodocs": "tag"
+//   }
+// };
+// export default config;
