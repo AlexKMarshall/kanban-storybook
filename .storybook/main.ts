34e00ddf3bd42465fb39ebc2cbeb5b114ec3a2f3
AlexKMarshall <alexmarshall@qogita.com>
2023-02-13 07:38:35 +0000

    chore: remove boilerplate

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,25 +21,3 @@ const config: StorybookConfig = {
 };
 
 export default config;
-
-// import type { StorybookConfig } from '@storybook/react-vite';
-
-// const config: StorybookConfig = {
-//   "stories": [
-//     "../stories/**/*.mdx",
-//     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-//   ],
-//   "addons": [
-//     "@storybook/addon-links",
-//     "@storybook/addon-essentials",
-//     "@storybook/addon-interactions"
-//   ],
-//   "framework": {
-//     "name": "@storybook/react-vite",
-//     "options": {}
-//   },
-//   "docs": {
-//     "autodocs": "tag"
-//   }
-// };
-// export default config;
